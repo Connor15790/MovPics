cb619bf0b5d4d33045bec8cc790bbdb3c8096156
Connor15790 <ConnorKenway157@gmail.com>
2024-06-25 06:48:58 +0530

    more changes

--- client/src/Pages/VideoPage/VideoPage.jsx
+++ client/src/Pages/VideoPage/VideoPage.jsx
@@ -170,7 +170,7 @@ function VideoPage() {
               </div>
               <video
                 // src={`http://localhost:5500/${vv?.filePath}`}
-                src={`https://mov-.app/${vv?.filePath}`}
+                src={`https://mov-pics.vercel.app/${vv?.filePath}`}
                 className={"video_ShowVideo_videoPage"}
                 ref={videoRef}
                 autoPlay
